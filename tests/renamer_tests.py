d2bcfd2e2f64aced58bc41867e226c94fe16c9eb
Jonathon Chambers <jonathon@jonathonchambers.com>
2019-12-04 06:27:19 -0500

    Started Creating tests

---
+++ tests/renamer_tests.py
@@ -0,0 +1,8 @@
+import unittest
+from ..renamer import Renamer
+
+class TestRenamer(unittest.TestCase):
+    def setUp(self):
+        self.Renamer = Renamer()
+    def test_csv_process(self):
+        pass
